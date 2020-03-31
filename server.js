6dad990673dc74a0f218208dff973210cd59a117
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-04-01 00:37:43 +0200

    MongoDB hooked in

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 
 dotenv.config({ path: "./config.env" })
+
+const DB = process.env.MONGO_ACCESS_STRING.replace(
+  "<PASSWORD>",
+  process.env.MONGO_PASSWORD
+)
+
+mongoose
+  .connect(DB, {
+    //options object - settings dealing with deprication warnings:
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+    // .connect() returns a promise
+  })
+  .then(con => {
+    // with .then we have access to connection obj: con
+    console.log(
+      `DB successfuly connected... \nuser: ${con.connections[0].user}`
+    )
+  })
+  .catch(() => {
+    console.log("DB connection failed")
+  })
 
 const app = require("./app")
 
