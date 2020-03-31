b50410ff327285f9f4013fd76cde246b61143a52
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-03-31 23:15:49 +0200

    routing imlpemented

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express")
 
 const app = express()
+const co2Router = require("./router/co2Router")
+
+// Middleware cycle:
+app.use(express.json()) // <-- body parser
+// -- routing
+app.use("/api/v1/co2", co2Router)
 
 module.exports = app
---
+++ controller/co2Controller.js
@@ -0,0 +1,28 @@
+exports.getLatestCo2 = (req, res) => {
+  res
+    .status(200)
+    .json(
+      "GET request for LATEST CO2 received. Controller not yet implemented..."
+    )
+}
+exports.updateLatestCo2 = (req, res) => {
+  res
+    .status(200)
+    .json(
+      "PUT request for LATEST CO2 received. Controller not yet implemented..."
+    )
+}
+exports.getDailyCo2 = (req, res) => {
+  res
+    .status(200)
+    .json(
+      "GET request for DAILY CO2 received. Controller not yet implemented..."
+    )
+}
+exports.updateDailyCo2 = (req, res) => {
+  res
+    .status(200)
+    .json(
+      "PUT request for DAILY CO2 received. Controller not yet implemented..."
+    )
+}
