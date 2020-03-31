ec722d27ff0ffe58a0543c7edf8db1fced43c8d7
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-04-01 01:17:34 +0200

    Mongoose schema and model added/test data inserted

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv")
 
 dotenv.config({ path: "./config.env" })
 
+const app = require("./app")
+
 const DB = process.env.MONGO_ACCESS_STRING.replace(
   "<PASSWORD>",
   process.env.MONGO_PASSWORD
@@ -27,7 +29,36 @@ mongoose
     console.log("DB connection failed")
   })
 
-const app = require("./app")
+// Mongoose constructor:
+// --- schema:
+const chartDataSchema = new mongoose.Schema({
+  labels: {
+    type: Array,
+    required: true
+  },
+  values: {
+    type: Array,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  }
+})
+// --- model:
+const chartDataModel = mongoose.model("ChartData", chartDataSchema)
+// --- instance/document:
+const TestData = new chartDataModel({
+  labels: [],
+  values: [],
+  title: "testchartdata"
+})
+// --- execution:
+// --- .save() returns a promise with access to newly created document
+TestData.save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(err))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}......`))
