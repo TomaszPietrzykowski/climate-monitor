52667ca795f3e2e084e89164259cf3af506c8835
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-06-01 00:09:56 +0200

    cron schedule set

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
-const CronJob = require("cron").CronJob
 
+const cron = require("./controller/cronController")
 const globalErrorHandler = require("./controller/errorController")
 const chartDataRouter = require("./router/chartDataRouter")
-// const co2 = require("./controller/co2Controller")
-// const ch4 = require("./controller/ch4Controller")
-// const n2o = require("./controller/n2oController")
-// const sf6 = require("./controller/sf6Controller")
-const {
-  runDailyUpdate,
-  runMonthlyUpdate,
-  runAnnualCO2Update,
-  runAnnualOtherUpdate,
-} = require("./controller/cronController")
 
 dotenv.config({ path: "./config.env" })
 
@@ -56,44 +46,9 @@ mongoose
   .catch(() => {
     console.log("DB connection failed")
   })
-// --------- cron sandbox -----------------------------------------
 
-const dailyUpdate = new CronJob(
-  "0 0 0,6,12,18 * * *",
-  runDailyUpdate,
-  null,
-  true,
-  "America/Los_Angeles"
-)
-
-const monthlyUpdate = new CronJob(
-  "0 1 2 * * *",
-  runMonthlyUpdate,
-  null,
-  true,
-  "America/Los_Angeles"
-)
-
-const annualCO2Update = new CronJob(
-  "0 2 4 0,3 * *",
-  runAnnualCO2Update,
-  null,
-  true,
-  "America/Los_Angeles"
-)
-
-const annualOtherUpdate = new CronJob(
-  "0 3 5 1,4 * *",
-  runAnnualOtherUpdate,
-  null,
-  true,
-  "America/Los_Angeles"
-)
-
-dailyUpdate.start()
-monthlyUpdate.start()
-annualCO2Update.start()
-annualOtherUpdate.start()
+// run data update schedule
+cron.run()
 
 const PORT = process.env.PORT || 5000
 const server = app.listen(PORT, () =>
--- controller/cronController.js
+++ controller/cronController.js
@@ -1,3 +1,4 @@
+const CronJob = require("cron").CronJob
 const co2 = require("./co2Controller")
 const ch4 = require("./ch4Controller")
 const n2o = require("./n2oController")
@@ -5,15 +6,14 @@ const sf6 = require("./sf6Controller")
 
 // -----------------------
 
-exports.runDailyUpdate = () => {
-  console.log(" --------- new cron cycle ------")
+const runDailyUpdate = () => {
   co2.readDailyCO2()
   setTimeout(() => {
     co2.readWeeklyCO2()
   }, 10000)
 }
 
-exports.runMonthlyUpdate = () => {
+const runMonthlyUpdate = () => {
   co2.readMonthlyCO2ML()
   setTimeout(() => {
     co2.readMonthlyCO2GL()
@@ -29,7 +29,7 @@ exports.runMonthlyUpdate = () => {
   }, 10000)
 }
 
-exports.runAnnualCO2Update = () => {
+const runAnnualCO2Update = () => {
   co2.readAnnualCO2ML()
   setTimeout(() => {
     co2.readAnnualCO2GL()
@@ -42,7 +42,7 @@ exports.runAnnualCO2Update = () => {
   }, 10000)
 }
 
-exports.runAnnualOtherUpdate = () => {
+const runAnnualOtherUpdate = () => {
   ch4.readAnnualCH4()
   setTimeout(() => {
     ch4.readAnnualGrowthRateCH4()
@@ -61,4 +61,40 @@ exports.runAnnualOtherUpdate = () => {
   }, 10000)
 }
 
-// -------------------------------------------------------------------
+const dailyUpdate = new CronJob(
+  "0 0 0,6,12,18 * * *",
+  runDailyUpdate,
+  null,
+  true,
+  "America/Los_Angeles"
+)
+
+const monthlyUpdate = new CronJob(
+  "0 15 3 * * *",
+  runMonthlyUpdate,
+  null,
+  true,
+  "America/Los_Angeles"
+)
+
+const annualCO2Update = new CronJob(
+  "0 30 4 1,7,14,28 * *",
+  runAnnualCO2Update,
+  null,
+  true,
+  "America/Los_Angeles"
+)
+
+const annualOtherUpdate = new CronJob(
+  "0 45 5 2,5,12,27 * *",
+  runAnnualOtherUpdate,
+  null,
+  true,
+  "America/Los_Angeles"
+)
+exports.run = () => {
+  dailyUpdate.start()
+  monthlyUpdate.start()
+  annualCO2Update.start()
+  annualOtherUpdate.start()
+}
