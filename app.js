98716247c622e4470a7bb3b06057aae3f07d3fff
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2020-08-13 01:25:17 +0200

    cors added

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv")
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const cron = require("./controller/cronController")
-const globalErrorHandler = require("./controller/errorController")
-const chartDataRouter = require("./router/chartDataRouter")
-const publicApiRouter = require("./router/publicApiRouter")
-const logger = require("./Logger")
+const cron = require("./controller/cronController");
+const globalErrorHandler = require("./controller/errorController");
+const chartDataRouter = require("./router/chartDataRouter");
+const publicApiRouter = require("./router/publicApiRouter");
+const logger = require("./Logger");
 
-dotenv.config({ path: "./config.env" })
+dotenv.config({ path: "./config.env" });
 
-const app = express()
+const app = express();
 
 // Middleware cycle:
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json()) // <-- body parser
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // <-- body parser
 
 // -- routing
-app.use("/api/v1/chartdata", chartDataRouter)
-app.use("/api/public/v1", publicApiRouter)
+app.use("/api/v1/chartdata", chartDataRouter);
+app.use("/api/public/v1", publicApiRouter);
 
 // catch all invalid routes - push err to error middleware by passing arg to next()
 app.all("*", (req, res, next) => {
-  next(new AppError(`Couldn't find ${req.originalUrl}`, 404))
-})
+  next(new AppError(`Couldn't find ${req.originalUrl}`, 404));
+});
 
 //global error handling middleware
-app.use(globalErrorHandler)
+app.use(globalErrorHandler);
 
 // -- db CONNECTION
 const DB = process.env.MONGO_ACCESS_STRING.replace(
   "<PASSWORD>",
   process.env.MONGO_PASSWORD
-)
+);
 
 mongoose
   .connect(DB, {
@@ -44,16 +46,16 @@ mongoose
   .then((con) => {
     console.log(
       `MongoDB successfuly connected...... \nDB user: ${con.connections[0].user}`
-    )
+    );
   })
   .catch(() => {
-    console.log("DB connection failed")
-  })
+    console.log("DB connection failed");
+  });
 
 // run data update schedule
-cron.run()
+cron.run();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () =>
   console.log(`Server running on port: ${PORT}......`)
-)
+);
