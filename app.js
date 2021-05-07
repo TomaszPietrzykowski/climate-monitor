a1dbb1a1d893e01cda8d5e4281e247bc06ab8edc
TomaszPietrzykowski <pietrzykowski77@gmail.com>
2021-05-07 20:14:07 +0200

    error boundries added to berkeley controller

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ cron.run()
 // sf6.readAnnualSF6()
 // sf6.readAnnualGrowthRateSF6()
 
-// ber.updateDailyTempAnomalyLS()
+ber.updateMonthlyTempAnomalyLOC()
 
 // news.updateNewsfeed();
 const PORT = process.env.PORT || 5000
--- controller/berkeleyController.js
+++ controller/berkeleyController.js
@@ -186,23 +186,25 @@ const parseDailyTempAnomaly = (inputData) => {
 // ----------------------------------------------------------------------------  UPDATE FUNCTIONS
 
 exports.updateAnnualTempAnomalyLS = async () => {
-  endpoints.forEach(async (e) => {
-    const data = await getBerkeley(`Complete_${e[0]}_summary.txt`)
-    const anomaly = parseAnnualTempAnomaly(data)
-    const temp = parseAnnualTemp(anomaly.chart, e[2])
+  try {
+    for (const e of endpoints) {
+      const data = await getBerkeley(`Complete_${e[0]}_summary.txt`)
+      const anomaly = parseAnnualTempAnomaly(data)
+      const temp = parseAnnualTemp(anomaly.chart, e[2])
 
-    // console.log(`temp_annual_anomaly_${e[1]}_public`)
-
-    updateDataset(`annual_land_temp_anomaly_${e[1]}`, anomaly.chart)
-    updateDataset(`annual_land_temp_${e[1]}`, temp.chart)
-    updatePublicDataset(`temp_annual_anomaly_${e[1]}_public`, anomaly.public)
-    updatePublicDataset(`temp_annual_${e[1]}_public`, temp.public)
-  })
+      updateDataset(`annual_land_temp_anomaly_${e[1]}`, anomaly.chart)
+      updateDataset(`annual_land_temp_${e[1]}`, temp.chart)
+      updatePublicDataset(`temp_annual_anomaly_${e[1]}_public`, anomaly.public)
+      updatePublicDataset(`temp_annual_${e[1]}_public`, temp.public)
+    }
+  } catch (err) {
+    throw new AppError(`Error updating annual temp anomaly: ${err}`, 500)
+  }
 }
 
 exports.updateMonthlyTempAnomalyLS = async () => {
-  endpoints.forEach(async (e) => {
-    try {
+  try {
+    for (const e of endpoints) {
       const data = await getBerkeley(`Complete_${e[0]}_complete.txt`)
       const anomaly = parseMonthlyTempAnomaly(data)
       const temp = parseMonthlyTemp(anomaly.chart, e[3])
@@ -211,74 +213,86 @@ exports.updateMonthlyTempAnomalyLS = async () => {
       updateDataset(`monthly_land_temp_${e[1]}`, temp.chart)
       updatePublicDataset(`temp_monthly_anomaly_${e[1]}_public`, anomaly.public)
       updatePublicDataset(`temp_monthly_${e[1]}_public`, temp.public)
-    } catch (err) {
-      throw new AppError(`Gotya! ${err}`, 500)
     }
-  })
+  } catch (err) {
+    throw new AppError(`Error updating monthly temp anomaly: ${err}`, 500)
+  }
 }
 
 exports.updateDailyTempAnomalyLS = async () => {
-  for (const e of endpoints) {
-    const data = await getBerkeley(`Complete_${e[0]}_daily.txt`)
-    const anomaly = parseDailyTempAnomaly(data)
-    updateDataset(`daily_land_temp_anomaly_${e[1]}`, anomaly.chart)
-    updatePublicDataset(`temp_daily_${e[1]}_public`, anomaly.public)
+  try {
+    for (const e of endpoints) {
+      const data = await getBerkeley(`Complete_${e[0]}_daily.txt`)
+      const anomaly = parseDailyTempAnomaly(data)
+      updateDataset(`daily_land_temp_anomaly_${e[1]}`, anomaly.chart)
+      updatePublicDataset(`temp_daily_${e[1]}_public`, anomaly.public)
+    }
+  } catch (err) {
+    throw new AppError(`Error updating daily temp anomaly: ${err}`, 500)
   }
 }
 
 exports.updateAnnualTempAnomalyLOC = async () => {
-  const data = await getBerkeley(`Land_and_Ocean_summary.txt`)
-  const anomaly = parseAnnualTempAnomaly(data)
-  const temp = parseAnnualTemp(anomaly.chart, 14.18)
-  updateDataset(`annual_loc_temp_anomaly`, anomaly.chart)
-  updateDataset(`annual_loc_temp`, temp.chart)
-  updatePublicDataset(`temp_annual_loc_anomaly_public`, anomaly.public)
-  updatePublicDataset(`temp_annual_loc_public`, temp.public)
+  try {
+    const data = await getBerkeley(`Land_and_Ocean_summary.txt`)
+    const anomaly = parseAnnualTempAnomaly(data)
+    const temp = parseAnnualTemp(anomaly.chart, 14.18)
+    updateDataset(`annual_loc_temp_anomaly`, anomaly.chart)
+    updateDataset(`annual_loc_temp`, temp.chart)
+    updatePublicDataset(`temp_annual_loc_anomaly_public`, anomaly.public)
+    updatePublicDataset(`temp_annual_loc_public`, temp.public)
+  } catch (err) {
+    throw new AppError(`Error updating annual temp anomaly LOC: ${err}`, 500)
+  }
 }
 
 exports.updateMonthlyTempAnomalyLOC = async () => {
-  const data = await getBerkeley(`Land_and_Ocean_complete.txt`)
-  const filteredLabels = []
-  const filteredValues = []
-  const filteredUncertainty = []
+  try {
+    const data = await getBerkeley(`Land_and_Ocean_complete.txt`)
+    const filteredLabels = []
+    const filteredValues = []
+    const filteredUncertainty = []
 
-  const anomaly = parseMonthlyTempAnomaly(data)
-  anomaly.chart.labels.forEach((el, i) => {
-    if (filteredLabels.filter((lab) => lab === el).length === 0) {
-      filteredLabels.push(el)
-      filteredValues.push(anomaly.chart.values[i])
-      filteredUncertainty.push(anomaly.chart.uncertainty[i])
-    }
-  })
-  const filtered = {
-    labels: filteredLabels,
-    values: filteredValues,
-    uncertainty: filteredUncertainty,
-  }
-  const publicData = []
-  filtered.labels.forEach((label, i) => {
-    publicData.push({
-      label,
-      value: filtered.values[i],
-      uncertainty: filtered.uncertainty[i],
+    const anomaly = parseMonthlyTempAnomaly(data)
+    anomaly.chart.labels.forEach((el, i) => {
+      if (filteredLabels.filter((lab) => lab === el).length === 0) {
+        filteredLabels.push(el)
+        filteredValues.push(anomaly.chart.values[i])
+        filteredUncertainty.push(anomaly.chart.uncertainty[i])
+      }
     })
-  })
-  const temp = parseMonthlyTemp(filtered, [
-    12.29,
-    12.5,
-    13.12,
-    14.04,
-    15.02,
-    15.72,
-    16.0,
-    15.83,
-    15.24,
-    14.31,
-    13.29,
-    12.55,
-  ])
-  updateDataset(`monthly_loc_temp_anomaly`, filtered)
-  updateDataset(`monthly_loc_temp`, temp.chart)
-  updatePublicDataset(`temp_monthly_loc_anomaly_public`, publicData)
-  updatePublicDataset(`temp_monthly_loc_public`, temp.public)
+    const filtered = {
+      labels: filteredLabels,
+      values: filteredValues,
+      uncertainty: filteredUncertainty,
+    }
+    const publicData = []
+    filtered.labels.forEach((label, i) => {
+      publicData.push({
+        label,
+        value: filtered.values[i],
+        uncertainty: filtered.uncertainty[i],
+      })
+    })
+    const temp = parseMonthlyTemp(filtered, [
+      12.29,
+      12.5,
+      13.12,
+      14.04,
+      15.02,
+      15.72,
+      16.0,
+      15.83,
+      15.24,
+      14.31,
+      13.29,
+      12.55,
+    ])
+    updateDataset(`monthly_loc_temp_anomaly`, filtered)
+    updateDataset(`monthly_loc_temp`, temp.chart)
+    updatePublicDataset(`temp_monthly_loc_anomaly_public`, publicData)
+    updatePublicDataset(`temp_monthly_loc_public`, temp.public)
+  } catch (err) {
+    throw new AppError(`Error updating monthly temp anomaly LOC: ${err}`, 500)
+  }
 }
